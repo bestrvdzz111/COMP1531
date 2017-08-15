d2bd6c58a11f9a57f57d89a42b7de1ae64d8bb38
bestrvdzz111 <824986031@qq.com>
2017-08-15 15:00:00 +1000

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, render_template, request, url_for
+from server import app, user_input
+import csv
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        zID = int(request.form["zID"])
+        description = request.form["desc"]
+         
+        user_input.append([name, zID, description])
+
+        with open('example.csv','a') as csv_out:
+           writer = csv.writer(csv_out)
+           writer.writerow([name, zID, description])
+
+        return redirect(url_for("hello"))
+    return render_template("index.html")
+
+@app.route("/Hello")
+def hello():
+   with open('example.csv','r') as csv_in:
+      reader = csv.reader(csv_in)
+      for row in reader:
+        print(row)
+
+      return render_template("hello.html", all_users=user_input)
